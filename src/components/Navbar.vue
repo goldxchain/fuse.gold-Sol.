ad944174512536494bd0e9dd80aa5a004b2e9640
vergil06 <vergilwilburn@gmail.com>
2021-08-31 15:02:41 -0700

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
 	<nav class="navbar navbar-expand-md wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.20s">
 		<div class="container">
 			<a class="navbar-brand" href="index.html"><img src="../assets/logo.png" alt="LOGO"></a>
+			<span>v 0.1.0</span>
 			<div class="nav-item dropdown lang-sl">
 				<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
 					<img src="../assets/icons/flag-01.png" alt="icon"> EN
