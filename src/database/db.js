8dd32c87b2261d66edd828b60c201279e20ff7c4
vergil06 <vergilwilburn@gmail.com>
2021-12-11 19:15:28 -0800

    commit

--- src/database/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    database: 'mongodb+srv://doadmin:v0918LNCo2PSx574@db-mongodb-lon1-fusegold-dev-263e49ba.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-lon1-fusegold-dev'
-};
